0a48103519ffe0eab80983e18ea53acd0018dc82
Prajesh Patel <prajesh22@gmail.com>
2017-04-19 16:28:20 -0400

    Removed .html

--- app/BlindEyeWebApp.py
+++ app/BlindEyeWebApp.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home')
 
 @app.route('/results/')
 def results():
-    return render_template('results.html')
+    return render_template('results')
 
 if __name__ == '__main__':
     app.run(debug=True)
