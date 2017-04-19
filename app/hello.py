92c96022f2ec80b2c57215cd7752b37a1e0f7df7
Prajesh Patel <prajesh22@gmail.com>
2017-04-19 17:54:47 -0400

    sdsdsd
    sdsdsd

--- app/hello.py
+++ app/hello.py
@@ -4,12 +4,12 @@ from flask import Flask, request
 
 from flask import Flask, render_template
 
-app = Flask('hello')
+app = Flask(__name__, template_folder='template')  # still relative to module
 # webcode = open('webcode.html').read() - not needed
 
 @app.route('/')
-def webprint():
-    return render_template('results.html') 
+def home():
+    return render_template('results.html')
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 3000)
